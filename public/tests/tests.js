1e57493c2ccfa44510da593799d2d435289a7e05
alu4073 <alu0100536836@ull.edu.es>
2014-03-16 19:42:58 +0000

    Añadido un test y modificado el estilo de mocha a scss

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -1,7 +1,9 @@
 var assert = chai.assert;
 
 suite('Resultados de los tests: ', function() {
-   test('Prueba', function() {
-	   assert.match("HOLA", /OL/);
-   });
+   test('Generación correcta de plantilla', function() {
+        original.value = "if Nombre == Antonio then Genero = Hombre";
+        main();
+		    assert.equal(OUTPUT.innerHTML,'{\n  "type": "IF",\n  "left": {\n    "type": "==",\n    "left": {\n      "type": "ID",\n      "value": "Nombre"\n    },\n    "right": {\n      "type": "ID",\n      "value": "Antonio"\n    }\n  },\n  "right": {\n    "type": "=",\n    "left": {\n      "type": "ID",\n      "value": "Genero"\n    },\n    "right": {\n      "type": "ID",\n      "value": "Hombre"\n    }\n  }\n}');
+    });
 });
